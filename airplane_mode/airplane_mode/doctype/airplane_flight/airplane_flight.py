f921c198006bfedc9843d7e3dff5f2c04fd4edd4
nidhi-boghara <nidhi.boghara@aavatto.com>
2025-08-25 10:57:52 +0530

    Day 4 Integration changes

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -9,18 +9,19 @@ from frappe.model.document import Document
 class AirplaneFlight(WebsiteGenerator):
 	
 	def on_submit(self):
+		print('on submit call-----------------------------')
 		self.status = "Completed"
 
 	def on_cancel(self):
 		self.status = "Cancelled"
 
-	def on_update(self):
-		if self.gate_number:
-			tickets = frappe.get_all("Airplane Ticket", filters={"flight": self.name}, fields=["name"])
-			if tickets:
-				for ticket in tickets:
-					ticket_doc = frappe.get_doc("Airplane Ticket", ticket["name"])
-					ticket_doc.gate_number = self.gate_number
-					ticket_doc.save()
+def update_gate_number(doc,method):
+	if doc.gate_number:
+		tickets = frappe.get_all("Airplane Ticket", filters={"flight": doc.name}, fields=["name"])
+		if tickets:
+			for ticket in tickets:
+				ticket_doc = frappe.get_doc("Airplane Ticket", ticket["name"])
+				ticket_doc.gate_number = doc.gate_number
+				ticket_doc.save()
 
-				frappe.msgprint(f"Gate number updated in all linked tickets for flight {self.name}.")
+			frappe.msgprint(f"Gate number updated in all linked tickets for flight {doc.name}.")
--- airplane_mode/hooks.py
+++ airplane_mode/hooks.py
@@ -137,13 +137,11 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+	"Airplane Flight": {
+		"on_update": "airplane_mode.airplane_mode.doctype.airplane_flight.airplane_flight.update_gate_number",
+	}
+}
 
 # Scheduled Tasks
 # ---------------
